62c7c72977a0c2344f6f82a3e7734ca65cac94d9
Durga Prasad Mamidi <durgaprasad.mamidi1608@gmail.com>
2022-09-19 18:55:48 -0700

    Initial Commit

--- src/main/java/org/mamidi/springbootquickstart/Main.java
+++ src/main/java/org/mamidi/springbootquickstart/Main.java
@@ -1,4 +1,6 @@
-package org.mamidi.springbootquickstart;public class Main {
+package org.mamidi.springbootquickstart;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
